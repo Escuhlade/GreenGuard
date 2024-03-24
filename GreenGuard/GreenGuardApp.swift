12f6f0c819cd43c9d34f063bedf1e569d37447a3
Aaiyan Virji <aaiyanvirji@gmail.com>
2024-03-24 18:23:54 -0400

    Initial Commit

---
+++ GreenGuard/GreenGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenGuardApp.swift
+//  GreenGuard
+//
+//  Created by Aaiyan Virji on 2024-03-24.
+//
+
+import SwiftUI
+
+@main
+struct GreenGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
